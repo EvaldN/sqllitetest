29520cb0f8c79d669880c6bb45559047b8a0575b
Evald <91316779+EvaldN@users.noreply.github.com>
2023-05-22 11:44:50 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=sqldb1", option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products", "test");
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(k => k.Id);
+                entity.HasIndex(i => i.Name).IsUnique();
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp2;
+
+    var dbName = "sqldb1.db";
+    if(File.Exists(dbName))
+    {
+        File.Delete(dbName);
+    }
+    await using var dbContext = new SqliteDbContext();
+    await dbContext.Database.EnsureCreatedAsync();
+    await dbContext.Products.AddRangeAsync(new Product[]
+    {
+        new Product(){Name = "Apple", Price = 10.99},
+        new Product(){Name = "Bomba", Price = 50.99},
+        new Product(){Name = "Banana", Price = 12.99},
+    });
+    await dbContext.SaveChangesAsync();
+
+    Console.WriteLine("getting database data");
+    dbContext.Products?.ToList().ForEach(p => Console.WriteLine(p.Name + " costs " + p.Price));
+    Console.ReadLine();
